65df9f51a9ceb9db661fc4b580e743043a1d3b46
dzakiluqman <dzakiluqman275@gmail.com>
2026-05-10 17:47:44 +0700

    feat: Implement authentication features in Navbar, add Dashboard and Profile pages for talents

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log("Cek URL:", import.meta.env.PUBLIC_SUPABASE_URL);
+console.log("Cek Key:", import.meta.env.PUBLIC_SUPABASE_ANON_KEY);
+
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 );
